ae10262f5fd0a7626e33780c2aa974a13fbf6df4
Steve Stone <steveostudios@gmail.com>
2018-07-03 19:27:10 +0000

    Update bookshelf.js

---
+++ src/pages/bookshelf.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const BookshelfPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <h2>2018</h2>
+    <ul>
+        <li>Remote</li>
+        <li>Conversational Design</li>
+        <li>The New CSS Layout</li>
+        <li>JavaScript: The Good Parts</li>
+        <li>Rocket Surgery Made Easy</li>
+        <li>Practical Design Discovery</li>
+        <li>High Performance JavaScript</li>
+    </ul>
+  </div>
+)
+
+export default BookshelfPage
