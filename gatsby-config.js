60ef24c77c4d7b3895f8ed507c6ee791835aedba
Steve Stone <steveostudios@gmail.com>
2018-07-05 14:11:49 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
 module.exports = {
   pathPrefix: `/`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-52636837-4",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"]
+      }
+    }
+  ]
+};
