1fd9971333be8f38289ab7334e7bdc820a8e1fa9
Steve Stone <steveostudios@gmail.com>
2018-07-07 15:08:09 -0400

    background is responsive!

--- src/layouts/Background/index.js
+++ src/layouts/Background/index.js
@@ -5,12 +5,19 @@ class Background extends React.Component {
   render() {
     return (
       <div className="Background">
-        <div className="shape-1" />
-        <div className="shape-2" />
-        <div className="shape-3" />
-        <div className="shape-4" />
-        <div className="shape-5" />
-        <div className="shape-6" />
+        <div className="shape shape-1 magenta right" />
+        <div className="shape shape-2 yellow right" />
+        <div className="shape shape-3 cyan right" />
+        <div className="shape shape-4 key right" />
+        <div className="shape shape-5 key-25 right" />
+        <div className="shape shape-6 stripes right" />
+
+        <div className="shape shape-7 magenta" />
+        <div className="shape shape-8 yellow" />
+        <div className="shape shape-9 cyan" />
+        <div className="shape shape-10 key" />
+        <div className="shape shape-11 key-25" />
+        <div className="shape shape-12 stripes" />
       </div>
     );
   }
