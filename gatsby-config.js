30c46953020a8096ca444b9a51e085b5a855ad35
Steve Stone <steveostudios@gmail.com>
2018-07-08 21:14:07 -0400

    more cleanup and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,24 @@ module.exports = {
       options: {
         fonts: [`Rubik`, `Nunito`]
       }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
     }
   ]
 };
