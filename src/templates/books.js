4ec59d0c1ec614bb6f7589374141e861b148ec8d
Steve Stone <steve@downloadyouthministry.com>
2020-09-16 06:37:21 -0400

    make it a single column. It's better this way.

--- src/templates/books.js
+++ src/templates/books.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
-import BookList from "../components/book-list";
+// import BookList from "../components/book-list";
+import Img from "gatsby-image";
+import "./books.css";
 
 const BooksTemplate = () => {
   const data = useStaticQuery(graphql`
@@ -30,9 +32,70 @@ const BooksTemplate = () => {
 
   const books = data.allBooksYaml.nodes;
 
+  const booksByYear = books
+    .filter((book) => book.dateFinish)
+    .sort((a, b) => new Date(b.dateFinish) - new Date(a.dateFinish))
+    .reduce((acc, cur) => {
+      console.log(acc[cur["dateFinish"]]);
+      acc[new Date(cur["dateFinish"]).getFullYear()] = [
+        ...(acc[new Date(cur["dateFinish"]).getFullYear()] || []),
+        cur,
+      ];
+      return acc;
+    }, {});
+
   return (
     <Layout>
-      <BookList books={books} />
+      <ul className="books">
+        <li>
+          <h5>Currently Reading</h5>
+          <ul className="books-year">
+            {books
+              .filter((book) => !book.dateFinish)
+              .map((book) => (
+                <li key={book.id} className="book">
+                  <Img
+                    fluid={book.image.childImageSharp.fluid}
+                    alt={book.title}
+                  />
+                  <div className="progress-bar">
+                    <span
+                      style={{
+                        width: 100 * (book.progress / book.pages) + "%",
+                      }}
+                    />
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </li>
+        {Object.entries(booksByYear)
+          .reverse()
+          .map(([year, books]) => (
+            <li key={year} className="year">
+              <h5>
+                {year}
+                <span>books {books.length}</span>
+                <span>
+                  pages{" "}
+                  {books
+                    .map((book) => book.pages)
+                    .reduce((acc, cur) => acc + cur)}
+                </span>
+              </h5>
+              <ul className="books-year">
+                {books.map((book) => (
+                  <li key={book.id} className="book">
+                    <Img
+                      fluid={book.image.childImageSharp.fluid}
+                      alt={book.title}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+      </ul>
     </Layout>
   );
 };
--- src/templates/home.js
+++ src/templates/home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Img from "gatsby-image";
+
+import { graphql, useStaticQuery } from "gatsby";
+
 // import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
 import PersonalityEnneagram from "./../components/personailty-enneagram";
@@ -10,10 +14,47 @@ import EducationHistory from "./../components/education-history";
 import "./home.css";
 
 const HomeTemplate = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "steveostudios.jpg" }) {
+        childImageSharp {
+          fixed(width: 144, height: 144) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
   return (
     <Layout>
-      <div>
-        <About />
+      <div className="top">
+        <div className="sidebar">
+          <div className="avatar">
+            <div className="swatch" />
+            <Img
+              className="headshot"
+              fixed={data.file.childImageSharp.fixed}
+              alt="headshot"
+            />
+          </div>
+          <h2>Developer. Designer. Youth Worker. Nerd.</h2>
+          <div className="location">
+            <div>
+              <svg
+                height="128"
+                viewBox="0 0 128 128"
+                width="128"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="m68.9329793 125.417448c36.3243767-52.6595419 43.0667917-58.0640701 43.0667917-77.4172954 0-26.5097024-21.490183-47.99988557-47.9998854-47.99988557-26.5097025 0-47.9998856 21.49018317-47.9998856 47.99988557 0 19.3532253 6.7424156 24.7577535 43.0667919 77.4172954 2.3835392 3.443352 7.482404 3.443596 9.8661874 0zm-4.9330937-57.4173431c-11.045628 0-19.9999524-8.9543244-19.9999524-19.9999523 0-11.045628 8.9543244-19.9999523 19.9999524-19.9999523 11.0456279 0 19.9999523 8.9543243 19.9999523 19.9999523 0 11.0456279-8.9543244 19.9999523-19.9999523 19.9999523z" />
+              </svg>
+            </div>
+            <div>Louisville, KY</div>
+          </div>
+        </div>
+        <div>
+          <About />
+        </div>
       </div>
       <div className="history">
         <WorkHistory />
